b79ab8fcdcecaa9a7a16abc4c0f4035ffc409065
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-07-27 11:12:24 +0200

    add configuration file

--- main.go
+++ main.go
@@ -9,15 +9,22 @@ import (
 	"github.com/jgsqware/termitask/view/git"
 	"github.com/jgsqware/termitask/view/tasks"
 	tui "github.com/marcusolsson/tui-go"
+	"github.com/spf13/viper"
 )
-
-var repos = []string{"/Users/juliengarciagonzalez/workspace/go/src/github.com/giantswarm/opsctl",
-	"/Users/juliengarciagonzalez/workspace/go/src/github.com/giantswarm/leanix-exporter",
-	"/Users/juliengarciagonzalez/workspace/go/src/github.com/jgsqware/clairctl"}
 
 func main() {
 
-	ght := os.Getenv("GITHUB_AUTH_TOKEN")
+	viper.SetConfigName("termitask")
+	viper.AddConfigPath("$HOME/.termitask")
+	viper.AddConfigPath(".")
+	viper.BindEnv("github_auth_token", "GITHUB_AUTH_TOKEN")
+	err := viper.ReadInConfig()
+	viper.WatchConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
+
+	ght := viper.GetString("github_auth_token")
 
 	if ght == "" {
 		fmt.Println("GITHUB_AUTH_TOKEN no set")
@@ -35,7 +42,7 @@ func main() {
 
 	tb := tasks.NewTaskBox(*ui, "Regular Tasks", "Ctrl+r", "Ctrl+f", "Ctrl+c")
 	repoB := []tui.Widget{}
-	for _, v := range repos {
+	for _, v := range viper.GetStringSlice("repos") {
 		repoB = append(repoB, git.NewGitBox(*ui, v))
 	}
 	t := tui.SimpleFocusChain{}
